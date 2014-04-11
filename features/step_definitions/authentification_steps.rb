f878c6ead5ba6d2f95d09fc592eb9fa0aac791bc
ljnissen <lj_nissen@gmx.de>
2014-04-11 22:32:22 +0200

    Finish sign in

---
+++ features/step_definitions/authentification_steps.rb
@@ -0,0 +1,33 @@
+Given /^a user visits the signin page$/ do
+  visit signin_path
+end
+
+When /^they submit invalid signin information$/ do
+  click_button "Sign in"
+end
+
+Then(/^they should see an error message$/) do
+  pending # express the regexp above with the code you wish you had end
+end
+Then(/^they should see a signout line$/) do
+  pending # express the regexp above with the code you wish you had end
+end
+
+Given /^the user has an account$/ do
+  @user = User.create(name: "Example User", email: "user@example.com",
+                      password: "foobar", password_confirmation: "foobar")
+end
+
+When /^the user submits valid signin information$/ do
+  fill_in "Email",    with: @user.email
+  fill_in "Password", with: @user.password
+  click_button "Sign in"
+end
+
+Then /^they should see their profile page$/ do
+  expect(page).to have_title(@user.name)
+end
+
+Then /^they should see a signout link$/ do
+  expect(page).to have_link('Sign out', href: signout_path)
+end
