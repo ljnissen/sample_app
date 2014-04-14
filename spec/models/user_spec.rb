51b7d3d4ee797f3328c632bd3307ff6bead5e254
ljnissen <lj_nissen@gmx.de>
2014-04-14 13:38:22 +0200

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com",  
-                    password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -17,6 +17,8 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -54,7 +56,7 @@ describe User do
         expect(@user).not_to be_valid
       end
     end
-end
+  end
 
   describe "when email format is valid" do
     it "should be valid" do
@@ -66,7 +68,7 @@ end
     end
   end
   
-   describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -75,46 +77,76 @@ end
   end
 
   describe "when password is not present" do
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
                      password: " ", password_confirmation: " ")
-  end
-
+    end
 
     it { should_not be_valid }
   
-end
-describe "when password doesn't match confirmation" do
-  before { @user.password_confirmation = "mismatch" }
-  it { should_not be_valid }
-end
-
-it { should respond_to(:authenticate) }
-
-describe "with a password that's too short" do
-  before { @user.password = @user.password_confirmation = "a" * 5 }
-  it { should be_invalid }
-end
-
-describe "return value of authenticate method" do
-  before { @user.save }
-  let(:found_user) { User.find_by(email: @user.email) }
-
-  describe "with valid password" do
-    it { should eq found_user.authenticate(@user.password) }
+  end
+  
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
   end
 
-  describe "with invalid password" do
-    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    it { should respond_to(:authenticate) }
 
-    it { should_not eq user_for_invalid_password }
-    specify { expect(user_for_invalid_password).to be_false }
-  end
-   describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
-  end
+    describe "with a password that's too short" do
+        before { @user.password = @user.password_confirmation = "a" * 5 }
+        it { should be_invalid }
+    end
 
+    describe "return value of authenticate method" do
+        before { @user.save }
+        let(:found_user) { User.find_by(email: @user.email) }
 
-end
+        describe "with valid password" do
+          it { should eq found_user.authenticate(@user.password) }
+        end
+
+        describe "with invalid password" do
+          let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+          it { should_not eq user_for_invalid_password }
+          specify { expect(user_for_invalid_password).to be_false }
+        end
+    
+        describe "remember token" do
+          before { @user.save }
+          its(:remember_token) { should_not be_blank }
+        end
+    end
+
+          describe "micropost associations" do
+
+            before { @user.save }
+            let!(:older_micropost) do
+              FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+            end
+            let!(:newer_micropost) do
+              FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+            end
+ 
+              it "should destroy associated microposts" do
+                microposts = @user.microposts.to_a
+                @user.destroy
+                expect(microposts).not_to be_empty
+                microposts.each do |micropost|
+                  expect(Micropost.where(id: micropost.id)).to be_empty
+                end
+              end
+        
+
+            describe "status" do
+              let(:unfollowed_post) do
+                FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+              end
+
+                its(:feed) { should include(newer_micropost) }
+                its(:feed) { should include(older_micropost) }
+                its(:feed) { should_not include(unfollowed_post) }
+            end
+          end
 end
